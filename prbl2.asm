c7b3cfa34fd6c5be93d729adeda22ffaa5bd2369
Gabi240400 <62253444+Gabi240400@users.noreply.github.com>
2021-06-05 03:29:28 +0300

    Add files via upload

--- prbl2.asm
+++ prbl2.asm
@@ -1,42 +1,52 @@
+;2/(a+b*c-9)+e-d
+;a,b,c - byte
+;d,e - doubleword
+
 assume cs:code, ds:data
+
 data segment
-	my_buffer db 100 dup (?)
-	linie_noua db 10,13,'$'
+a db 11
+b db -5
+c db 8
+d dd 15
+e dd -20
+rez dd ?
 data ends
 
 code segment
 start:
-	mov ax,data
+	mov ax, data
 	mov ds,ax
 
-	mov si,0 ;pentru adaugarea caracterelor in buffer
-	citeste:
-		mov ah,08h
-		int 21h
-		;al = codul ascii al caracterului citit
-		cmp al,48
-		JB adauga_buffer
-		cmp al,57
-		JA adauga_buffer
-		;daca caracterul citit este cifra, il tiparesc
-		mov dl,al
-		mov ah,02h
-		int 21h
-		;daca caracterul citit nu este cifra il adaug in buffer
-		JMP citeste
-		adauga_buffer:
-			mov my_buffer[si],al
-			inc si
-			cmp al,36
-			JE end_citire
-			JMP citeste
-	;afisez bufferul
-	end_citire:
-		mov dx,offset linie_noua
-		mov ah,09h
-		int 21h
-		mov dx,offset my_buffer
-		int 21h
+	mov al,b
+	imul c ;ax=b*c
+	mov bx,ax ;bx=b*c
+	mov al,a
+	cbw ;ax=a
+	add ax,bx ;ax=a+b*c
+	sub ax,9 ;ax=a+b*c-9
+	mov bx,ax ;bx=a+b*c-9
+	mov ax,2
+	mov dx,0
+	idiv bx 
+	;in dx am restul si in ax catul
+	cwd ;dx:ax=2/(a+b*c-9)
+	;mut e in cx:bx
+	mov bx,word ptr e+0
+	mov cx,word ptr e+2 ;cx:bx=e
+	clc
+	add ax,bx
+	adc dx,cx ;dx:ax=dx:ax+cx:bx=2/(a+b*c-9)+e
+	;mut d in cx:bx
+	mov bx,word ptr d+0
+	mov cx,word ptr d+2 ;cx:bx=d
+	clc
+	sub ax,bx
+	sbb dx,cx ;dx:ax=dx:ax-cx:bx=2/(a+b*c-9)+e-d
+	;mut dx:ax in rez
+	mov word ptr rez+0,ax
+	mov word ptr rez+2,dx ;rez=2/(a+b*c-9)+e-d
+
 	mov ax,4C00h
 	int 21h
 code ends
